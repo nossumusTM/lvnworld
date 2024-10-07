09caa07e43a10b1c8a798af2571d1d5477e48a36
Nossumus™ <info.lvnmmdv@gmail.com>
2024-10-07 17:36:43 +0400

    Latest update

--- src/app/javascript/Application.js
+++ src/app/javascript/Application.js
@@ -375,9 +375,7 @@ return () => {
 }, [playerId]);
 
 return (
-    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
       <canvas ref={canvasRef} className="canvas js-canvas" />
-    </div>
   );
 };
 
